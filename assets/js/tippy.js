0fd5ff9441fc696247582e77e114d4418db03b69
Kostya Prodan <debaun4ry@gmail.com>
2024-09-10 10:34:04 +0300

    Add files via upload

--- assets/js/tippy.js
+++ assets/js/tippy.js
@@ -1,3 +1,11 @@
 tippy('#hero-prompt', {
 	content: 'Hello world!',
 });
+
+tippy('#time-stomp-prompt', {
+	content: 'Hello world!',
+});
+
+tippy('#ai-about-prompt', {
+	content: 'Hello world!',
+});
